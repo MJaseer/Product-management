9fadc4152ce3675addfc32ef743fe7b5dd3509b5
Jaseer-Synram <m.jaseer@synram.co>
2024-12-19 00:17:07 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'product-management-ionic',
+  webDir: 'www'
+};
+
+export default config;
